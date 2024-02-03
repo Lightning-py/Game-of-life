32581506d5d186d87743bf1eebb12a77d5c0f0da
Carylon Florencio <carylonf@et-b3.kzn.21-school.ru>
2024-02-03 16:58:16 +0300

    Распределение задач

--- src/game_of_life.c
+++ src/game_of_life.c
@@ -1,8 +1,9 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <time.h>
 
-#define FIELD_HEIGHT 1
-#define FIELD_WIDTH 10
+#define FIELD_HEIGHT 25
+#define FIELD_WIDTH 80
 
 /*
  * компилировать код только с флагами -Wall -Werror -Wextra
@@ -12,9 +13,9 @@
 int** allocate();       // выделяет память для массива
 int** create_matrix();  // создает массив игрового поля 80x25 клеток и читает поле со стандартного ввода
 int get_alive_neighbours(int** field, int x,
-                         int y);  // возвращает количество живых клеток вокруг данной, принимает координаты
-                                  // клетки (необходимо учитывать то, что края поля соединены)
-void update_field(int*** field);  // расчитывает следующий шаг игры и записывает в int*** field
+                         int y);  // возвращает количество живых клеток вокруг данной, принимает
+                                  // координаты клетки (необходимо учитывать то, что края поля соединены)
+void update_field(int** field);  // расчитывает следующий шаг игры и записывает в int*** field
 void display(int** field);  // отображает текущий шаг на экране
 
 int get_right_index_x(int x);
@@ -25,25 +26,28 @@ void free_field(int** array);  // освобождает память
 int main() {
     int** matr = create_matrix();
 
-    while (1) {
+    int steps = 0;
+
+    while (steps < 2) {
+        update_field(matr);
+
         display(matr);
 
-        printf("haha");
-
-        update_field(&matr);
+        delay(10);
+        steps++;
     }
 
     return 0;
 }
 
-void update_field(int*** field) {
+void update_field(int** field) {
     int** new_array = allocate();
 
     for (int i = 0; i < FIELD_HEIGHT; ++i) {
         for (int j = 0; j < FIELD_WIDTH; ++j) {
-            int nb = get_alive_neighbours(*field, i, j);
+            int nb = get_alive_neighbours(field, j, i);
 
-            if ((*field)[i][j] == 0)  // dead
+            if ((field)[i][j] == 0)  // dead
             {
                 if (nb == 3) new_array[i][j] = 1;
             } else  // alive
@@ -58,7 +62,7 @@ void update_field(int*** field) {
 
     for (int i = 0; i < FIELD_HEIGHT; ++i) {
         for (int j = 0; j < FIELD_WIDTH; ++j) {
-            (*field)[i][j] = new_array[i][j];
+            (field)[i][j] = new_array[i][j];
         }
     }
 
@@ -67,9 +71,12 @@ void update_field(int*** field) {
 
 int get_alive_neighbours(int** field, int x, int y) {
     int count = 0;
+
     for (int dx = -1; dx <= 1; ++dx) {
         for (int dy = -1; dy <= 1; ++dy) {
-            if (field[get_right_index_x(x + dx)][get_right_index_y(y + dy)] == 1) count++;
+            int x_ind = get_right_index_x(x + dx);
+            int y_ind = get_right_index_y(y + dy);
+            if (field[y_ind][x_ind] == 1) count++;
         }
     }
 
@@ -77,20 +84,18 @@ int get_alive_neighbours(int** field, int x, int y) {
 }
 
 int get_right_index_x(int x) {
-    if (x == -1) return FIELD_WIDTH;
-    if (x == FIELD_WIDTH + 1) return 0;
+    if (x == -1) return FIELD_WIDTH - 1;
+    if (x == FIELD_WIDTH) return 0;
     return x;
 }
 
 int get_right_index_y(int y) {
-    if (y == -1) return FIELD_HEIGHT;
+    if (y == -1) return FIELD_HEIGHT - 1;
     if (y == FIELD_HEIGHT) return 0;
     return y;
 }
 
 void display(int** field) {
-    printf("\e[1;1H\e[2J");
-
     for (int i = 0; i < FIELD_HEIGHT; ++i) {
         for (int j = 0; j < FIELD_WIDTH; ++j) {
             if (field[i][j] == 1)
@@ -98,7 +103,7 @@ void display(int** field) {
             else
                 printf(" ");
         }
-        printf("\nlol");
+        printf("\n");
     }
 }
 
