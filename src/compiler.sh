9d71737985da84e934e7157537222ee0f0b1fce8
Sergey <sergejananev78@gmail.com>
2024-02-12 20:56:08 +0300

    Compliler

--- src/compiler.sh
+++ src/compiler.sh
@@ -1,10 +1,8 @@
 #!bin/bash
 
-path=$1
-gcc -Wall -Wextra -Werror -lncurses $path
-clang-format -i $path
-clang-format -n $path
+gcc -Wall -Wextra -Werror -lncurses game_of_life.c -o game_of_life 
+clang-format -i *.c *.h 
 
-cppcheck --enable=all --suppress=missingIncludeSystem $path            
+cppcheck --enable=all --suppress=missingIncludeSystem *.c *.h            
 echo "all right, boss!"
 
