7a90d4c41888390578462eb438d874bb40a6edf5
Sergey <sergejananev78@gmail.com>
2024-02-12 21:09:41 +0300

    Finale

--- src/compiler.sh
+++
@@ -1,8 +0,0 @@
-#!bin/bash
-
-gcc -Wall -Wextra -Werror -lncurses game_of_life.c -o game_of_life 
-clang-format -i *.c *.h 
-
-cppcheck --enable=all --suppress=missingIncludeSystem *.c *.h            
-echo "all right, boss!"
-
